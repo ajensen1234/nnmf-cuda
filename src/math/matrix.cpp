214939be7ffe7d0de472e92939ae2b2f21382147
Andrew Jensen <63751488+ajensen1234@users.noreply.github.com>
2022-09-29 16:38:06 -0400

    Matrix ops and NNMF multiplicative update

--- src/math/matrix.cpp
+++ src/math/matrix.cpp
@@ -66,6 +66,21 @@ void matrix::mat_mult(matrix* A, matrix*B, matrix* out){
     }
 }
 
+matrix* matrix::mat_mult(matrix* A, matrix* B){
+    matrix* out = new matrix(A->get_num_rows(),B->get_num_cols());
+    for (int i = 0; i<A->get_num_rows();i++){
+        for (int j = 0; j<B->get_num_cols(); j++){
+            float sum = 0;
+            for(int k = 0; k < A->get_num_cols();k++){
+                sum+= (A->at(i,k))*(B->at(k,j));
+            }
+            out->set(i,j,sum);
+        }
+    }
+    return out;
+}
+
+
 void matrix::load_data(std::string filename, const char delimiter){
 
     int nrows = csv_reader::countLines(filename);
@@ -91,13 +106,33 @@ void matrix::load_data(std::string filename, const char delimiter){
     
 }
 
+void matrix::set_equal_to(matrix* new_matrix){
+    for (int i = 0; i<get_num_rows();i++){
+        for(int j = 0; j<get_num_cols();j++){
+            set(i,j,new_matrix->at(i,j));
+        }
+    }
+}
 
 void matrix::print_matrix(){
-    get_num_cols();
+
     for (int i = 0; i<numrows_;i++){
         for (int j = 0; j < numcols_;j++){
-            std::cout << this->at(i,j) << ", ";
+            std::cout << at(i,j) << ", ";
         }
         std::cout << std::endl;
     }
+}
+
+matrix* matrix::transpose(){
+    if (!transpose_){
+        std::cout <<"Pointer was null (1)" << std::endl;
+    transpose_ = new matrix(numcols_,numrows_);
+    for(int i =0; i<numrows_;i++){
+        for (int j = 0; j < numcols_; j++){
+            transpose_->set(j,i,at(i,j));
+        }
+    }
+    } else std::cout << "Pointer was not null (2)" << std::endl;
+    return transpose_;
 }
